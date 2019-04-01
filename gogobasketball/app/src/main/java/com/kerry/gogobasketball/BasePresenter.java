b5c238211b8220954433876c3506721abe93e63e
KerryChen <chen081501@hotmail.com>
2019-04-01 16:58:41 +0800

    Based on Stylish, create activity main layout, drawable, mipmap, strings, styles, colors

---
+++ gogobasketball/app/src/main/java/com/kerry/gogobasketball/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.kerry.gogobasketball;
+
+public interface BasePresenter {
+
+    void start();
+
+}
---
+++ gogobasketball/app/src/main/java/com/kerry/gogobasketball/BaseView.java
@@ -0,0 +1,7 @@
+package com.kerry.gogobasketball;
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+}
